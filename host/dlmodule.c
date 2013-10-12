af3e01b16e44d8063da9e93ccefe5fe941456d45
lvsgame <lvsgame@sina.cn>
2013-10-12 12:25:04 +0800

    del debug

--- host/dlmodule.c
+++ host/dlmodule.c
@@ -18,8 +18,6 @@ _open(struct dlmodule* dl) {
         host_error("dlmodule %s open error: %s", dl->name, dlerror());
         return 1;
     } 
-    dlerror();
-    printf("open dl->name:%s, %p\n", dl->name, handle);
     size_t len = strlen(dl->name);
     strcpy(tmp, dl->name);
     strcpy(tmp+len, "_create");
@@ -61,9 +59,7 @@ static void
 _dlclose(struct dlmodule* dl) {
     if (dl->handle == NULL)
         return;
-    printf("dl->name:%s, %p\n", dl->name, dl->handle);
-    //if (strcmp("log", dl->name) !=0)
-        dlclose(dl->handle);
+    dlclose(dl->handle);
     dl->handle = NULL;
     dl->create = NULL;
     dl->free = NULL;
