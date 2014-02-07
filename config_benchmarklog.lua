525a547a5df0fa6dfacb442f22d4592d539dbc45
lvsgame <lvsgame@sina.cn>
2014-02-07 18:15:29 +0800

    fix bug

---
+++ config_benchmarklog.lua
@@ -0,0 +1,10 @@
+node_id = 7
+node_ip = "127.0.0.1"
+node_port = 8006
+center_ip   = "127.0.0.1"
+center_port = 8000
+
+sc_loglevel = "INFO"
+sc_connmax = 256
+sc_service = "log,node,cmdctl,benchmarklog"
+benchmarklog_times_persec=10000
---
+++ config_gamelog.lua
@@ -0,0 +1,9 @@
+node_id = 8
+node_ip = "127.0.0.1"
+node_port = 8007
+center_ip   = "127.0.0.1"
+center_port = 8000
+
+sc_loglevel = "INFO"
+sc_connmax = 256
+sc_service = "log,node,cmdctl,gamelog"
