753c1cbda6f22134dee261d751cbbb36324e0c40
lvsgame <lvxiaojun@debian.(none)>
2014-02-10 20:45:45 +0800

    fix gamelog

--- mod/mod_gamelog.c
+++ mod/mod_gamelog.c
@@ -1,34 +1,69 @@
 #include "sh.h"
 #include "elog_include.h"
+#include <pthread.h>
+#include <unistd.h>
 
-struct gamelog {
-    struct elog *logger; 
-    int count;
-    uint64_t last_time;
+struct log_entry {
+    void *msg;
+    int sz;
+    struct log_entry *next;
 };
 
-struct gamelog *
-gamelog_create() {
-    struct gamelog *self = malloc(sizeof(*self));
-    memset(self, 0, sizeof(*self));
-    return self;
-}
+struct log_tailq {
+    struct log_entry *head;
+    struct log_entry *tail;
+    pthread_mutex_t mutex;
+};
 
-void
-gamelog_free(struct gamelog *self) {
-    if (self == NULL)
-        return;
-    elog_free(self->logger);
-    self->logger = NULL;
-    free(self);
-}
+struct gamelog {
+    volatile bool worker_run;
+    bool worker_ok;
+    pthread_t worker_id; 
+    struct log_tailq log_queue;
+    struct elog *logger; 
+    uint64_t last_time;
+    int count;
+};
 
-int
-gamelog_init(struct module *s) {
-    struct gamelog *self = MODULE_SELF;
-    if (sh_handle_publish(MODULE_NAME, PUB_SER)) {
-        return 1;
+// log
+static int
+log_push(struct log_tailq *q, void *msg, int sz) {
+    struct log_entry *log = malloc(sizeof(*log));
+    log->msg = msg;
+    log->sz = sz;
+    log->next = NULL;
+   
+    pthread_mutex_lock(&q->mutex);
+    if (q->head) {
+        assert(q->tail != NULL);
+        assert(q->tail->next == NULL);
+        q->tail->next = log;
+    } else {
+        q->head = log;
     }
+
+    q->tail = log; 
+    pthread_mutex_unlock(&q->mutex);
+    return 0;
+}
+
+static struct log_entry *
+log_pop(struct log_tailq *q) {
+    pthread_mutex_lock(&q->mutex);
+    if (q->head) {
+        struct log_entry *log = q->head;
+        q->head = log->next;
+        pthread_mutex_unlock(&q->mutex);
+        return log;
+    } else {
+        pthread_mutex_unlock(&q->mutex);
+        return NULL;
+    }
+    
+}
+
+static int 
+create_log(struct gamelog *self) {
     struct elog *logger = elog_create("/tmp/gamelog.log");
     if (logger == NULL) {
         return 1;
@@ -41,16 +76,93 @@ gamelog_init(struct module *s) {
     return 0;
 }
 
+static void *
+log_main(void *arg) {
+    struct gamelog *self = arg;
+    for (;;) {
+        struct log_entry *log = log_pop(&self->log_queue);
+        if (log) {
+            elog_append(self->logger, log->msg, log->sz);
+        } else {
+            if (self->worker_run) {
+                usleep(1000);
+            } else {
+                break;
+            }
+        }
+    }
+    return NULL;
+}
+
+static int
+create_logger(struct gamelog *self) {
+    if (create_log(self)) {
+        return 1;
+    }
+    self->log_queue.mutex = PTHREAD_MUTEX_INITIALIZER;
+    self->worker_run = true;
+
+    pthread_t tid;
+    int result = pthread_create(&tid, NULL, log_main, self);
+    if (result) {
+        sh_error("Create log thread(err#%d)", result);
+        return 1;
+    }
+    self->worker_ok = true;
+    self->worker_id = tid;
+    sh_info("Create log thread(%lu)", self->worker_id);
+    return 0;
+}
+
+static void
+destroy_logger(struct gamelog *self) {
+    if (self->worker_ok) {
+        self->worker_run = false;
+        pthread_join(self->worker_id, NULL);
+        self->worker_ok = false;
+    }
+    if (self->logger) {
+        elog_free(self->logger);
+        self->logger = NULL;
+    }
+}
+
+// gamelog
+struct gamelog *
+gamelog_create() {
+    struct gamelog *self = malloc(sizeof(*self));
+    memset(self, 0, sizeof(*self));
+    return self;
+}
+
+void
+gamelog_free(struct gamelog *self) {
+    if (self == NULL)
+        return;
+    destroy_logger(self);    
+    free(self);
+}
+
+int
+gamelog_init(struct module *s) {
+    struct gamelog *self = MODULE_SELF;
+    if (sh_handle_publish(MODULE_NAME, PUB_SER)) {
+        return 1;
+    } 
+    if (create_logger(self)) {
+        return 1;
+    }
+    return 0;
+}
+
 void
 gamelog_main(struct module *s, int session, int source, int type, const void *msg, int sz) {
     struct gamelog *self = MODULE_SELF;
     assert(type == MT_TEXT);
    
-    char tmp[1025];
-    int size = min(sz, sizeof(tmp)-1);
-    memcpy(tmp, msg, size);
-    tmp[size] = '\0';
-    elog_append(self->logger, tmp, size);
+    char* log = malloc(sz);
+    memcpy(log, msg, sz);
+    log_push(&self->log_queue, log, sz);
 
     self->count++;
     if (self->last_time == 0) {
