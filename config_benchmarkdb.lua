1f097b3f968dde533d4ddf107a218d7d88c2717b
lvsgame <lvxiaojun@debian.(none)>
2014-02-12 10:55:56 +0800

    benchmarkdb config

---
+++ config_benchmarkdb.lua
@@ -0,0 +1,12 @@
+node_id = 9
+node_ip = "127.0.0.1"
+node_port = 8010
+center_ip   = "127.0.0.1"
+center_port = 8000
+
+sh_loglevel = "INFO"
+sh_connmax = 256
+sh_module = "log,node,cmdctl,benchmarkdb"
+
+benchmark_query_init = 0
+benchmark_query = 0
