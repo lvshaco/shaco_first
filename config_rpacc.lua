cde12c62c3158e7329c06f8668dfd861a122e3dd
lvsgame <lvxiaojun@debian.(none)>
2014-03-20 14:32:13 +0800

    rm garbage

--- config_rpacc.lua
+++
@@ -1,9 +0,0 @@
-require "config_base"
-def_node("rpacc", 200)
-
-sh_module=sh_module..",redisproxy:rpacc"
-
-redis_auth="shaco@1986#0621"
-redis_requester="auth"
-
-redis_list="127.0.0.1:6379"
--- config_rprank.lua
+++
@@ -1,9 +0,0 @@
-require "config_base"
-def_node("rprank", 202)
-
-sh_module=sh_module..",redisproxy:rprank"
-
-redis_auth="shaco@1986#0621"
-redis_requester="hall"
-
-redis_list="127.0.0.1:6379"
